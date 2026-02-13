49b0d7b98f742168c0b880e503f0c805168d74ca
ItanW-Dev <itanwilczinski@gmail.com>
2026-02-13 14:30:43 +0000

    add models and views

---
+++ models/employee.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
+
+const employee = {
+
+  store: new JsonStore('./models/employee.json', { employee: {} }),
+  collection: 'employee',
+  array: 'creators',
+
+  getAppInfo() {
+    return this.store.findAll(this.collection);
+  },
+
+};
+
+export default employee;
