8a5d42ed3d9784988cbff1e51f8d45142b2d8657
ItanW-Dev <itanwilczinski@gmail.com>
2026-03-07 22:03:47 +0000

    first update

--- controllers/about.js
+++ controllers/about.js
@@ -1,16 +1,17 @@
 'use strict';
 
 import logger from "../utils/logger.js";
-import employee from "../models/employee.js";
+import appStore from "../models/app-store.js";
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
     
-     const viewData = {
+   const viewData = {
       title: "Welcome to the Playlist app!",
-      info: employee.getAllEmployees()
+      info: appStore.getAppInfo()
     };
+    
     
     response.render('about', viewData);   
   },
--- public/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello from the Web App Dev lab!");
